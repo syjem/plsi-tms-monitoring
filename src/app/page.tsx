969d1895c780c0b64b1fdd853305da45bac48112
aljimsondev <aljimson.megrino@gmail.com>
2025-11-25 21:16:10 +0800

    Chore | added alert error on error occured instead of blocking whole application rendering where there's an error occured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { getClaims } from '@/app/actions/get-claims';
 import { getWorkLogs } from '@/app/actions/logs/get-work-logs';
 import { Header } from '@/components/header';
 import TabSection from '@/components/tabs';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertCircleIcon } from 'lucide-react';
 import React from 'react';
 
 export default async function Home() {
@@ -14,6 +16,7 @@ export default async function Home() {
     <React.Fragment>
       <Header user={user} />
       <HeroSection name={user.user_metadata.full_name} />
+      {error && <ErrorAlert error={error} />}
       <TabSection logs={data ?? []} />
     </React.Fragment>
   );
@@ -28,6 +31,18 @@ function HeroSection({ name }: { name: string }) {
       <h1 className="text-2xl font-extrabold text-gray-900 dark:text-gray-100 font-sans">
         Phillogix Systems Employee <br /> Monitoring
       </h1>
+    </div>
+  );
+}
+
+function ErrorAlert({ error }: { error: string }) {
+  return (
+    <div className="flex items-center justify-center">
+      <Alert variant="destructive" className="max-w-fit">
+        <AlertCircleIcon />
+        <AlertTitle>Internal Server Error</AlertTitle>
+        <AlertDescription>{error}</AlertDescription>
+      </Alert>
     </div>
   );
 }
