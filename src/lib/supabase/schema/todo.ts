d5fe6ec4a29633964cf24685238ba3a251723c6d
aljimsondev <aljimson.megrino@gmail.com>
2025-11-25 20:34:59 +0800

    Chore | updated schema implementation for example

---
+++ src/lib/supabase/schema/todo.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const todo = pgTable('todo', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  todo: text().notNull(),
+  created_at: timestamp().defaultNow(),
+  updated_at: timestamp().defaultNow(),
+});
+
+export type Todo = typeof todo.$inferSelect;
