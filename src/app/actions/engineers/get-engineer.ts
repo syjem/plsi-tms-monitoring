a03a168dbbc84adb42b8e1bfee7686dae5597c6c
aljimsondev <aljimson.megrino@gmail.com>
2025-11-30 21:58:56 +0800

    used error message

--- src/app/actions/engineers/get-engineer.ts
+++ src/app/actions/engineers/get-engineer.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { getClaims } from '@/app/actions/get-claims';
+import { ERRORS } from '@/constants/errors';
 import { EngineerController } from '@/lib/controller/engineer.controller';
 import { db } from '@/lib/supabase';
 import { withErrorHandler } from '@/utils/with-error-handler';
@@ -10,7 +11,7 @@ export async function getEngineerById(id: string) {
     const { user } = await getClaims();
 
     // throw an error if the request is not authenticated
-    if (!user) throw new Error('Unauthorized access of resources!');
+    if (!user) throw new Error(ERRORS.NOT_ALLOWED);
 
     // initialize controller
     const controller = new EngineerController(db);
