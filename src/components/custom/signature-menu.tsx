0ad91395fbcda929de5e1fa163e3f470a94f5118
aljimsondev <aljimson.megrino@gmail.com>
2025-11-30 17:51:01 +0800

    Added signature menu for setting engineer signature

---
+++ src/components/custom/signature-menu.tsx
@@ -0,0 +1,41 @@
+import SignaturePad from '@/components/signature-pad';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { DialogTitle } from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+
+function SignatureMenu({ children }: { children: ReactNode }) {
+  const handleSaveSignature = (signatureData: string) => {
+    try {
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogContent className="max-w-[425px] md:max-w-fit">
+          <DialogHeader>
+            <DialogTitle>My Signature</DialogTitle>
+            <DialogDescription>Enter your signature here!</DialogDescription>
+          </DialogHeader>
+          <div className="pb-4">
+            <SignaturePad
+              width={500}
+              height={300}
+              onSaveSignature={handleSaveSignature}
+            />
+          </div>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
+
+export default SignatureMenu;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
-"use client";
+'use client';
 
+import SignatureMenu from '@/components/custom/signature-menu';
+import { AppLogo } from '@/components/icons';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,17 +10,15 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTheme } from "next-themes";
-import { Fragment, useState } from "react";
-import { useRouter } from "next/navigation";
-import { AppLogo } from "@/components/icons";
-import { Loader, LogOut } from "lucide-react";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import { JwtPayload } from "@supabase/supabase-js";
-import { createClient } from "@/lib/supabase/client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+} from '@/components/ui/dropdown-menu';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+import { createClient } from '@/lib/supabase/client';
+import { JwtPayload } from '@supabase/supabase-js';
+import { Loader, LogOut } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { useRouter } from 'next/navigation';
+import { Fragment, useState } from 'react';
 
 export function Header({ user }: { user: JwtPayload }) {
   const router = useRouter();
@@ -29,9 +30,9 @@ export function Header({ user }: { user: JwtPayload }) {
   const avatarUrl = user.user_metadata.avatar_url;
 
   const initials = userName
-    ?.split(" ")
+    ?.split(' ')
     ?.map((word) => word[0])
-    ?.join("")
+    ?.join('')
     ?.toUpperCase();
 
   const handleSignout = async () => {
@@ -39,9 +40,9 @@ export function Header({ user }: { user: JwtPayload }) {
     try {
       const supabase = createClient();
       await supabase.auth.signOut();
-      router.push("/auth/login");
+      router.push('/auth/login');
     } catch (error) {
-      console.error("Error signing out:", error);
+      console.error('Error signing out:', error);
     } finally {
       setLoading(false);
     }
@@ -70,14 +71,20 @@ export function Header({ user }: { user: JwtPayload }) {
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
+            <SignatureMenu>
+              <DropdownMenuLabel className="cursor-default" role="button">
+                My Signature
+              </DropdownMenuLabel>
+            </SignatureMenu>
+            <DropdownMenuSeparator />
             <DropdownMenuLabel>
               <div className="flex items-center justify-between">
                 <Label htmlFor="dark-mode">Dark mode</Label>
                 <Switch
                   id="dark-mode"
-                  checked={theme === "dark"}
+                  checked={theme === 'dark'}
                   onCheckedChange={(checked) =>
-                    setTheme(checked ? "dark" : "light")
+                    setTheme(checked ? 'dark' : 'light')
                   }
                 />
               </div>
