9f6aace724e8f63704d6407320e0c78969468a49
aljimsondev <aljimson.megrino@gmail.com>
2025-11-30 21:45:02 +0800

    updated userId->user_id, fieldNumber -> field_number to match the old declared schema field

--- src/lib/supabase/schema/engineer.ts
+++ src/lib/supabase/schema/engineer.ts
@@ -21,14 +21,14 @@ export const engineers = pgTable(
     signature: text(), // Store base64 string
     phone: varchar({ length: 50 }),
     email: text(),
-    userId: uuid()
+    user_id: uuid()
       .references(() => users.id, {
         onDelete: 'cascade',
       })
       .notNull(),
-    fieldNumber: smallint(),
+    field_number: smallint(),
   },
-  (table) => [check('field_number_check', sql`${table.fieldNumber} > 0`)],
+  (table) => [check('field_number_check', sql`${table.field_number} > 0`)],
 );
 
 // Type inference
@@ -38,7 +38,7 @@ export type NewEngineer = typeof engineers.$inferInsert;
 // Relations
 export const engineersRelations = relations(engineers, ({ one }) => ({
   user: one(users, {
-    fields: [engineers.userId],
+    fields: [engineers.user_id],
     references: [users.id],
   }),
 }));
