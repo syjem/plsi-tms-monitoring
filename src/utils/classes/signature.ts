37ad905d4360cde36f0aa6f93fcb023d382bf305
aljimsondev <aljimson.megrino@gmail.com>
2025-12-01 21:08:46 +0800

    fix | updated clearCanvas internal method to clearRect

--- src/utils/classes/signature.ts
+++ src/utils/classes/signature.ts
@@ -256,7 +256,8 @@ export class Signature {
    */
   clearCanvas() {
     if (!this.canvas) return console.warn('Unable to get canvas!');
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   /**
