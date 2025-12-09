9c83f65995a5611e223808cd3a4d4360a6605545
aljimsondev <aljimson.megrino@gmail.com>
2025-12-09 20:57:38 +0800

    Fix | added extra check for adding signature action

--- src/app/actions/engineers/add-signature.ts
+++ src/app/actions/engineers/add-signature.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { getUser } from '@/app/actions/get-user';
+import { ERRORS } from '@/constants/errors';
 import { EngineerController } from '@/lib/controller/engineer.controller';
 import { db } from '@/lib/supabase';
 import { withErrorHandler } from '@/utils/with-error-handler';
@@ -10,6 +12,12 @@ export const addEngineerSignature = async (
 ) => {
   const result = await withErrorHandler(async () => {
     if (!userId) throw new Error('userId is required');
+
+    // check for user session and authorization
+    const user = await getUser();
+
+    // retrict further access when no user token found (possible exploit access)
+    if (!user) throw new Error(ERRORS.NOT_ALLOWED);
 
     // initialize controller
     const controller = new EngineerController(db);
