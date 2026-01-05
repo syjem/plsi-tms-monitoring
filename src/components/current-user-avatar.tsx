46f4979b085033f043869f1880d63dce5ebfba38
Jemuel Repoylo <syjem143@gmail.com>
2026-01-05 23:37:16 +0800

    refactor: remove signature handling  from CurrentUserAvatar

--- src/components/current-user-avatar.tsx
+++ src/components/current-user-avatar.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { getEngineerSignature } from '@/app/actions/profiles/get-signature';
-import SignatureMenu from '@/components/custom/signature-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
@@ -13,19 +11,12 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { createClient } from '@/lib/supabase/client';
 import { User } from '@supabase/supabase-js';
-import { useQuery } from '@tanstack/react-query';
 import { Loader, LogOut } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 export function CurrentUserAvatar({ user }: { user: User }) {
   const [loading, setLoading] = useState(false);
-  const [openSignatureDialog, setSignatureDialogState] = useState(false);
-  const { data, refetch, isFetching } = useQuery({
-    queryFn: () => getEngineerSignature(),
-    queryKey: [user],
-    refetchOnWindowFocus: false,
-  });
 
   const router = useRouter();
 
@@ -67,22 +58,6 @@ export function CurrentUserAvatar({ user }: { user: User }) {
             <address className="text-muted-foreground">{email}</address>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <SignatureMenu
-          isFetching={isFetching}
-          data={data}
-          open={openSignatureDialog}
-          refetch={refetch}
-          onOpenChange={setSignatureDialogState}
-        >
-          <DropdownMenuLabel
-            className="cursor-default"
-            role="button"
-            onClick={() => setSignatureDialogState((prev) => !prev)}
-          >
-            My Signature
-          </DropdownMenuLabel>
-        </SignatureMenu>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={handleSignout} className="font-medium">
           {loading ? (
