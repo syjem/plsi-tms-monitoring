10a00f6aa88f36488b90d115af7e60a4659bf2d8
Jemuel Repoylo <syjem143@gmail.com>
2026-01-13 23:01:31 +0800

    fix: update public route handling in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,11 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - privacy (privacy page)
+     * - terms (terms page)
+     * - how-it-works (how it works page)
+     * - any image file with extensions: svg, png, jpg, jpeg, gif, webp
      */
-    '/((?!_next/static|_next/image|favicon.ico|auth/oauth|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|privacy|terms|how-it-works|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
